e20cf9fe3fae947dd64798195862414c1be2e7ca
MatrixTele <80210649+MatrixTele@users.noreply.github.com>
2021-05-18 18:34:23 +0300

    Add files via upload

--- Matrix.lua
+++ Matrix.lua
@@ -3031,7 +3031,7 @@ return false
 end
 if text == "المالكين" and DevBot(msg) then
 local list = database:smembers(bot_id.."creator"..msg.chat_id_)
-t = "\n✫: قائمة المالكين \n — — — — — — — — — \n"
+t = "\n• قائمة المالكين \n — — — — — — — — — \n"
 for k,v in pairs(list) do
 local username = database:get(bot_id.."User:Name" .. v)
 if username then
@@ -3041,7 +3041,7 @@ t = t..""..k.."- (`"..v.."`)\n"
 end
 end
 if #list == 0 then
-t = "✫: لا يوجد مالكين"
+t = "• لا يوجد مالكين"
 end
 send(msg.chat_id_, msg.id_, t)
 return false
@@ -3056,12 +3056,12 @@ database:sadd(bot_id.."creator"..msg.chat_id_,admins[i].user_id_)
 end 
 end  
 end,nil)
-send(msg.chat_id_, msg.id_, "✫:  تم مسح المالكين")
+send(msg.chat_id_, msg.id_, "•  تم مسح المالكين")
 end
 if text == ("رفع مالك") and tonumber(msg.reply_to_message_id_) ~= 0 and DevBot(msg) then  
 function Function_Matrix(extra, result, success)
 database:sadd(bot_id.."creator"..msg.chat_id_, result.sender_user_id_)
-Reply_Status(msg,result.sender_user_id_,"reply","✫: تم ترقيته مالك")  
+Reply_Status(msg,result.sender_user_id_,"reply","• تم ترقيته مالك")  
 end
 tdcli_function ({ID = "GetMessage",chat_id_ = msg.chat_id_,message_id_ = tonumber(msg.reply_to_message_id_)}, Function_Matrix, nil)
 return false
@@ -3071,13 +3071,13 @@ local username = text:match("^رفع مالك @(.*)$")
 function Function_Matrix(extra, result, success)
 if result.id_ then
 if (result and result.type_ and result.type_.ID == "ChannelChatInfo") then
-send(msg.chat_id_,msg.id_,"✫: عذرا عزيزي المستخدم هاذا معرف قناة يرجى استخدام الامر بصوره صحيحه !")   
+send(msg.chat_id_,msg.id_,"• عذرا عزيزي المستخدم هاذا معرف قناة يرجى استخدام الامر بصوره صحيحه !")   
 return false 
 end      
 database:sadd(bot_id.."creator"..msg.chat_id_, result.id_)
 Reply_Status(msg,result.id_,"reply","?? : تم ترقيته مالك")  
 else
-send(msg.chat_id_, msg.id_,"✫: لا يوجد حساب بهاذا المعرف")
+send(msg.chat_id_, msg.id_,"• لا يوجد حساب بهاذا المعرف")
 end
 end
 tdcli_function ({ID = "SearchPublicChat",username_ = username}, Function_Matrix, nil)
@@ -3086,13 +3086,13 @@ end
 if text and text:match("^رفع مالك (%d+)$") and DevBot(msg) then  
 local userid = text:match("^رفع مالك (%d+)$") 
 database:sadd(bot_id.."creator"..msg.chat_id_, userid)
-Reply_Status(msg,userid,"reply","✫: تم ترقيته مالك")  
+Reply_Status(msg,userid,"reply","• تم ترقيته مالك")  
 return false
 end
 if text == ("تنزيل مالك") and tonumber(msg.reply_to_message_id_) ~= 0 and DevBot(msg) then  
 function Function_Matrix(extra, result, success)
 database:srem(bot_id.."creator"..msg.chat_id_, result.sender_user_id_)
-Reply_Status(msg,result.sender_user_id_,"reply","✫: تم تنزيله من المالكين")  
+Reply_Status(msg,result.sender_user_id_,"reply","• تم تنزيله من المالكين")  
 end
 tdcli_function ({ID = "GetMessage",chat_id_ = msg.chat_id_,message_id_ = tonumber(msg.reply_to_message_id_)}, Function_Matrix, nil)
 return false
@@ -3102,9 +3102,9 @@ local username = text:match("^تنزيل مالك @(.*)$")
 function Function_Matrix(extra, result, success)
 if result.id_ then
 database:srem(bot_id.."creator"..msg.chat_id_, result.id_)
-Reply_Status(msg,result.id_,"reply","✫: تم تنزيله من المالكين")  
+Reply_Status(msg,result.id_,"reply","• تم تنزيله من المالكين")  
 else
-send(msg.chat_id_, msg.id_,"✫: لا يوجد حساب بهاذا المعرف")
+send(msg.chat_id_, msg.id_,"• لا يوجد حساب بهاذا المعرف")
 end
 end
 tdcli_function ({ID = "SearchPublicChat",username_ = username}, Function_Matrix, nil)
@@ -3113,7 +3113,7 @@ end
 if text and text:match("^تنزيل مالك (%d+)$") and DevBot(msg) then  
 local userid = text:match("^تنزيل مالك (%d+)$") 
 database:srem(bot_id.."creator"..msg.chat_id_, userid)
-Reply_Status(msg,userid,"reply","✫: تم تنزيله من المالكين")  
+Reply_Status(msg,userid,"reply","• تم تنزيله من المالكين")  
 return false
 end
 if text == ("رفع منشئ اساسي") and tonumber(msg.reply_to_message_id_) ~= 0 and DevBot(msg) then  
