d7e75cc8efe6537618c7b1587abfe69e8716e571
MatrixTele <80210649+MatrixTele@users.noreply.github.com>
2021-05-23 19:44:55 +0300

    Add files via upload

--- Matrix.lua
+++ Matrix.lua
@@ -8904,13 +8904,8 @@ end
 return false
 end
 local Text =[[
-Wꫀꪶᥴꪮꪑꫀ 𝓽ꪮ 𝓽ꫝꫀ [Mꪖ𝓽𝘳𝓲᥊𝓽ꫀꪖꪑ] (t.me/Matrix_Source) ᥇ꪮ𝓽  🔽
- • 𝟎 •↺ Cleaners - المنظفين
- • 𝟏 •↺ Protection - الحماية
- • 𝟐 •↺ addictive - الادمنية
- • 𝟑 •↺ Managers - المدراء
- • 𝟒 •↺ originators - المنشئين
- • 𝟓 •↺ Developers - المطورين
+اهلا بك في بوت [Matrix Team](t.me/Matrix_Source)
+يمكنك استخدام هذه الاوامر 🔽
 ]]
 keyboard = {} 
 keyboard.inline_keyboard = {
@@ -10905,13 +10900,8 @@ end
 elseif Text and Text:match('(.*)/help') then
 if tonumber(Text:match('(.*)/help')) == tonumber(data.sender_user_id_) then
 local Teext =[[
-Wꫀꪶᥴꪮꪑꫀ 𝓽ꪮ 𝓽ꫝꫀ [Mꪖ𝓽𝘳𝓲᥊𝓽ꫀꪖꪑ] (t.me/Matrix_Source) ᥇ꪮ𝓽  🔽
- • 𝟎 •↺ Cleaners - المنظفين
- • 𝟏 •↺ Protection - الحماية
- • 𝟐 •↺ addictive - الادمنية
- • 𝟑 •↺ Managers - المدراء
- • 𝟒 •↺ originators - المنشئين
- • 𝟓 •↺ Developers - المطورين
+اهلا بك في بوت [Matrix Team](t.me/Matrix_Source)
+يمكنك استخدام هذه الاوامر 🔽
 ]]
 keyboard = {} 
 keyboard.inline_keyboard = {
