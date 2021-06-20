74e1b237c77581ba4a54272421514d611013916b
MatrixTele <80210649+MatrixTele@users.noreply.github.com>
2021-06-20 22:50:14 +0300

    Add files via upload

--- Matrix.lua
+++ Matrix.lua
@@ -3327,63 +3327,56 @@ end
 end,nil)
 send(msg.chat_id_, msg.id_, "*• تم مسح المالكين*")
 end
-if text == ("رفع مالك") and tonumber(msg.reply_to_message_id_) ~= 0 and DevBot(msg) then  
+if text == ("رفع مالك") and tonumber(msg.reply_to_message_id_) ~= 0 and DevBot(msg) and AhMedMember(msg) then 
 function Function_Matrix(extra, result, success)
-database:sadd(bot_id.."creator"..msg.chat_id_, result.sender_user_id_)
-Reply_Status(msg,result.sender_user_id_,"reply","⏬| تم ترقيته مالك")  
+database:sadd(bot_id.."Matrix:Basic:Constructoryyu23"..msg.chat_id_, result.sender_user_id_)
+Reply_Status(msg,result.sender_user_id_,"reply","💢┇تم ترقيته مالك")  
 end
 tdcli_function ({ID = "GetMessage",chat_id_ = msg.chat_id_,message_id_ = tonumber(msg.reply_to_message_id_)}, Function_Matrix, nil)
 return false
 end
-if text and text:match("^رفع مالك @(.*)$") and DevBot(msg) then  
+if text and text:match("^رفع مالك @(.*)$") and DevBot(msg) and AhMedMember(msg) then 
 local username = text:match("^رفع مالك @(.*)$")
 function Function_Matrix(extra, result, success)
 if result.id_ then
 if (result and result.type_ and result.type_.ID == "ChannelChatInfo") then
-send(msg.chat_id_,msg.id_,"• عذرا عزيزي المستخدم هاذا معرف قناة يرجى استخدام الامر بصوره صحيحه !")   
+send(msg.chat_id_,msg.id_,"💢┇عذرا عزيزي المستخدم هاذا معرف قناة يرجى استخدام الامر بصوره صحيحه !")   
 return false 
 end      
-database:sadd(bot_id.."creator"..msg.chat_id_, result.id_)
-Reply_Status(msg,result.id_,"reply","⏬| تم ترقيته مالك")  
+database:sadd(bot_id.."Matrix:Basic:Constructoryyu23"..msg.chat_id_, result.id_)
+Reply_Status(msg,result.id_,"reply","💢┇تم ترقيته مالك")  
 else
-send(msg.chat_id_, msg.id_,"*• لا يوجد حساب بهاذا المعرف*")
+send(msg.chat_id_, msg.id_,"💢┇لا يوجد حساب بهاذا المعرف")
 end
 end
 tdcli_function ({ID = "SearchPublicChat",username_ = username}, Function_Matrix, nil)
 return false
 end
-if text and text:match("^رفع مالك (%d+)$") and DevBot(msg) then  
-local userid = text:match("^رفع مالك (%d+)$") 
-database:sadd(bot_id.."creator"..msg.chat_id_, userid)
-Reply_Status(msg,userid,"reply","⏬| تم ترقيته مالك")  
-return false
-end
-if text == ("تنزيل مالك") and tonumber(msg.reply_to_message_id_) ~= 0 and DevBot(msg) then  
+if text == ("تنزيل مالك") and tonumber(msg.reply_to_message_id_) ~= 0 and DevBot(msg) and AhMedMember(msg) then 
 function Function_Matrix(extra, result, success)
-database:srem(bot_id.."creator"..msg.chat_id_, result.sender_user_id_)
-Reply_Status(msg,result.sender_user_id_,"reply","*⏬| تم تنزيله من المالكين*")  
+database:srem(bot_id.."Matrix:Basic:Constructoryyu23"..msg.chat_id_, result.sender_user_id_)
+Reply_Status(msg,result.sender_user_id_,"reply","💢┇تم تنزيله من المالك")  
 end
 tdcli_function ({ID = "GetMessage",chat_id_ = msg.chat_id_,message_id_ = tonumber(msg.reply_to_message_id_)}, Function_Matrix, nil)
-return false
+return false 
 end
-if text and text:match("^تنزيل مالك @(.*)$") and DevBot(msg) then  
+if text and text:match("^تنزيل مالك @(.*)$") and DevBot(msg) then 
+tdcli_function ({ID = "GetChatMember",chat_id_ = msg.chat_id_,user_id_ = msg.sender_user_id_},function(arg,da) 
+if da.status_.ID == "ChatMemberStatusCreator" then
 local username = text:match("^تنزيل مالك @(.*)$")
 function Function_Matrix(extra, result, success)
 if result.id_ then
-database:srem(bot_id.."creator"..msg.chat_id_, result.id_)
-Reply_Status(msg,result.id_,"reply","⏬| تم تنزيله من المالكين")  
+database:srem(bot_id.."Matrix:Basic:Constructoryyu23"..msg.chat_id_, result.id_)
+
+Reply_Status(msg,result.id_,"reply","💢┇تم تنزيله من المالكين")  
 else
-send(msg.chat_id_, msg.id_,"*• لا يوجد حساب بهاذا المعرف*")
+send(msg.chat_id_, msg.id_,"💢┇لا يوجد حساب بهاذا المعرف")
 end
 end
 tdcli_function ({ID = "SearchPublicChat",username_ = username}, Function_Matrix, nil)
 return false
 end
-if text and text:match("^تنزيل مالك (%d+)$") and DevBot(msg) then  
-local userid = text:match("^تنزيل مالك (%d+)$") 
-database:srem(bot_id.."creator"..msg.chat_id_, userid)
-Reply_Status(msg,userid,"reply","*⏬| تم تنزيله من المالكين*")  
-return false
+end,nil)   
 end
 if text == ("رفع منشئ اساسي") and tonumber(msg.reply_to_message_id_) ~= 0 and DevBot(msg) then  
 if AddChannel(msg.sender_user_id_) == false then
