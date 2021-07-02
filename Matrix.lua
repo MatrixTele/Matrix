ae9dffd30b480f009ad1015ccc26e813e02df953
MatrixTele <80210649+MatrixTele@users.noreply.github.com>
2021-07-02 17:03:55 +0300

    Add files via upload

--- Matrix.lua
+++ Matrix.lua
@@ -5113,7 +5113,7 @@ send(msg.chat_id_,msg.id_,' ['..ta.title_..']('..link..')')
 else                
 local linkgpp = json:decode(https.request('https://api.telegram.org/bot'..token..'/exportChatInviteLink?chat_id='..msg.chat_id_))
 if linkgpp.ok == true then 
-linkgp = '⛓꒐ 𝗴ٰ𝗿ٰ𝗼ٰ𝘂ٰ𝗽 ٰ𝗹ٰ𝗶ٰ𝗻ٰ𝗸 : /n  ━═━═━═━ /n ['..ta.title_..']('..linkgpp.result..')'
+linkgp = '\n*⛓꒐ 𝗴ٰ𝗿ٰ𝗼ٰ𝘂ٰ𝗽 ٰ𝗹ٰ𝗶ٰ𝗻ٰ𝗸 : *\n━═━═━═━\n ['..ta.title_..']('..linkgpp.result..')'
 else
 linkgp = '• لا يوجد رابط ارسل ضع رابط'
 end  
