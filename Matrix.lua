e067751617cd30d7bfb6afcdf44c9899fafeb8b6
MatrixTele <80210649+MatrixTele@users.noreply.github.com>
2021-07-10 19:52:16 +0300

    Add files via upload

--- Matrix.lua
+++ Matrix.lua
@@ -9976,10 +9976,8 @@ Reply_Status(msg,msg.sender_user_id_,"lock",'• تم تفعيل المسح ال
 return false
 end 
 if text == 'السورس' or text == 'الالعاب الاحترافيه' or text == 'الالعاب الاحترافية' or text == 'العاب احترافية' then  
-Text = [[*
-🐲 • Welcome to Source
-🚩 • TeAm - ”Matrix”*
- ••  ━═━═━═━••
+Text = [[
+ *• قائمه الالعاب الاحترافيه اضغط للعب*
 ]]
 keyboard = {} 
 keyboard.inline_keyboard = {
@@ -9987,10 +9985,52 @@ keyboard.inline_keyboard = {
 {text = '♟ Chess Game ♟',url="https://t.me/T4TTTTBOT?game=chess"},
 },
 {
-{text = '📁┇Matrix Files .',url="https://t.me/infoo_Matrix"},
+{text = 'لعبة فلابي بيرد 🐥',url="https://t.me/awesomebot?game=FlappyBird"},{text = 'تحداني فالرياضيات 🔢',url="https://t.me/gamebot?game=MathBattle"},
 },
 {
-{text = '📮 • Bot TwisL',url="t.me/U41bot"},
+{text = 'تحداني في ❌⭕️',url="t.me/XO_AABOT?start3836619"},{text = 'سباق الدراجات 🏍',url="https://t.me/gamee?game=MotoFX"},
+},
+{
+{text="سباق سيارات 🏎",url='https://t.me/gamee?game=F1Racer'},{text="متشابه 👾",url='https://t.me/gamee?game=DiamondRows'},
+},
+{
+{text="كرة قدم ⚽",url='https://t.me/gamee?game=FootballStar'}
+},
+{
+{text="دومنا🥇",url='https://vipgames.com/play/?affiliateId=wpDom/#/games/domino/lobby'},{text="❕ليدو",url='https://vipgames.com/play/?affiliateId=wpVG#/games/ludo/lobby'}
+},
+{
+{text="ورق🤹‍♂",url='https://t.me/gamee?game=Hexonix'},{text="Hexonix❌",url='https://t.me/gamee?game=Hexonix'}
+},
+{
+{text="MotoFx🏍️",url='https://t.me/gamee?game=MotoFx'}
+},
+{
+{text="لعبة 2048 🎰",url='https://t.me/awesomebot?game=g2048'},{text="Squares🏁",url='https://t.me/gamee?game=Squares'}
+},
+{
+{text="Atomic 1▶️",url='https://t.me/gamee?game=AtomicDrop1'},{text="Corsairs",url='https://t.me/gamebot?game=Corsairs'}
+},
+{
+{text="LumberJack",url='https://t.me/gamebot?game=LumberJack'}
+},
+{
+{text="LittlePlane",url='https://t.me/gamee?game=LittlePlane'},{text="RollerDisco",url='https://t.me/gamee?game=RollerDisco'}
+},
+{
+{text="🦖 Dragon Game 🦖",url='https://t.me/T4TTTTBOT?game=dragon'},{text="🐍 3D Snake Game 🐍",url='https://t.me/T4TTTTBOT?game=snake'}
+},
+{
+{text="🔵 Color Game 🔴",url='https://t.me/T4TTTTBOT?game=color'}
+},
+{
+{text="🚀 Rocket Game 🚀",url='https://t.me/T4TTTTBOT?game=rocket'},{text="🏹 Arrow Game 🏹",url='https://t.me/T4TTTTBOT?game=arrow'}
+},
+{
+{text = '- Matrix Team', url="t.me/Matrix_Source"}
+},
+{
+{text = 'مطور السورس', url="t.me/IZlZ7I"}
 },
 }
 local msg_id = msg.id_/2097152/0.5
