0c8b4fee4b33d712787b00982121d0c0088c7216
MatrixTele <80210649+MatrixTele@users.noreply.github.com>
2021-07-22 00:12:45 +0300

    Add files via upload

--- Matrix.lua
+++ Matrix.lua
@@ -11554,7 +11554,7 @@ if Text_Dev then
 send(msg.chat_id_, msg.id_,Text_Dev)
 else
 tdcli_function ({ID = "GetUser",user_id_ = Sudo},function(arg,data) 
-send(msg.chat_id_, msg.id_,"\n*🕷꒐ 𝖽𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋*\n━═━═━═━\n ["..data.first_name_.."](T.me/"..data.username_..") *←*")  
+send(msg.chat_id_, msg.id_,"\n*⌔︙ᗪEᐯEᒪOᑭEᖇ :*\n*┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉*\n ["..data.first_name_.."](T.me/"..data.username_..") *←*")  
 end,nil)   
 end
 end
