47414afb1c4d132aa8eecd1695237abb978182d8
MatrixTele <80210649+MatrixTele@users.noreply.github.com>
2021-07-23 10:47:54 +0300

    Add files via upload

--- Matrix.lua
+++ Matrix.lua
@@ -7721,7 +7721,7 @@ keyboard.inline_keyboard = {
 {{text="نعم",callback_data="/yesdel"},{text="لا",callback_data="/nodel"}}
 } 
 local msg_id = msg.id_/2097152/0.5 
-https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&sendMessage='..URL.escape(sendMessage.info)..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..Msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
 --     Source David     --
 if text == "تعطيل نزلني" and Owner(msg) then
