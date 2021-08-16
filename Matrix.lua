cbb89f5eb7111f1529fc779adb1290a8f427485d
MatrixTele <80210649+MatrixTele@users.noreply.github.com>
2021-08-16 22:10:28 +0300

    Add files via upload

--- Matrix.lua
+++ Matrix.lua
@@ -10365,11 +10365,11 @@ keyboard.inline_keyboard = {
 },
 }
 local msg_id = msg.id_/2097152/0.5
-local texte = '*⌔┆'..Description..'*\n⌔┆ايديك : '..Id..'*\n*⌔┆يوزرك : '..UserName_User..'*\n*⌔┆موقعك : '..Status_Gps..'*\n*⌔┆رسائلك : '..NumMsg..' *\n*⌔┆تفاعلك : '..TotalMsg..'*\n*⌔┆الالعاب : '..Num_Games..'*'
+local texte = '⌔┆'..Description..'\n⌔┆ايديك : '..Id..'\n⌔┆يوزرك : '..UserName_User..'\n⌔┆موقعك : '..Status_Gps..'\n⌔┆رسائلك : '..NumMsg..' \n⌔┆تفاعلك : '..TotalMsg..'\n⌔┆الالعاب : '..Num_Games..''
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id='..msg.chat_id_..'&caption='..URL.escape(texte)..'&photo='..Matrixteam.photos_[0].sizes_[1].photo_.persistent_id_..'&reply_to_message_id='..msg_id..'&disable_web_page_preview=true&reply_markup='..JSON.encode(keyboard)) 
 end
 else
-local texte = '\n*⌔┆ايديك : '..Id..'*\n*⌔┆يوزرك : ['..UserName_User..']*\n*⌔┆موقعك : '..Status_Gps..'*\n*⌔┆رسائلك : '..NumMsg..' *\n*⌔┆تفاعلك : '..TotalMsg..'*\n*⌔┆الالعاب : '..Num_Games..'*'
+local texte = '\n⌔┆ايديك : '..Id..'\n⌔┆يوزرك : ['..UserName_User..']\n⌔┆موقعك : '..Status_Gps..'\n⌔┆رسائلك : '..NumMsg..' \n⌔┆تفاعلك : '..TotalMsg..'\n⌔┆الالعاب : '..Num_Games..''
 keyboard = {} 
 keyboard.inline_keyboard = {
 {
@@ -10395,7 +10395,7 @@ local texte = '['..get_id..']'
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(texte).."&reply_to_message_id="..msg_id.."&parse_mode=markdown")
 else
-local texte = '\n*⌔┆ايديك : '..Id..'*\n*⌔┆يوزرك : ['..UserName_User..']*\n*⌔┆موقعك : '..Status_Gps..'*\n*⌔┆رسائلك : '..NumMsg..' *\n*⌔┆تفاعلك : '..TotalMsg..'*\n*⌔┆الالعاب : '..Num_Games..'*'
+local texte = '\n⌔┆ايديك : '..Id..'\n⌔┆يوزرك : ['..UserName_User..']\n⌔┆موقعك : '..Status_Gps..'\n⌔┆رسائلك : '..NumMsg..' \n⌔┆تفاعلك : '..TotalMsg..'\n⌔┆الالعاب : '..Num_Games..''
 keyboard = {} 
 keyboard.inline_keyboard = {
 {
